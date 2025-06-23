6233e56ea08f0981afa51200c6967c22de99ff25
Guste Gaubaite <219.guste@gmail.com>
2025-06-23 19:54:00 +0200

    test(spinner): add test to verify Loader renders inside Modal when active

--- src/common/components/spinner/spinner.component.spec.tsx
+++ src/common/components/spinner/spinner.component.spec.tsx
@@ -21,24 +21,35 @@ describe('SpinnerComponent', () => {
         toBeInDocument: true,
         description: 'render when promiseInProgress is true',
       },
-    ])(
-      'should $description when promise tracking is $promiseInProgress',
-      ({ promiseInProgress, toBeInDocument }) => {
-        //Arrange
-        mockUsePromiseTracker.mockReturnValue({ promiseInProgress });
+    ])('should $description', ({ promiseInProgress, toBeInDocument }) => {
+      //Arrange
+      mockUsePromiseTracker.mockReturnValue({ promiseInProgress });
 
-        //Act
-        render(<SpinnerComponent />);
+      //Act
+      render(<SpinnerComponent />);
 
-        //Assert
-        if (toBeInDocument) {
-          const spinner = screen.getByRole('presentation');
-          expect(spinner).toBeInTheDocument();
-        } else {
-          const spinner = screen.queryByRole('presentation');
-          expect(spinner).not.toBeInTheDocument();
-        }
+      //Assert
+      if (toBeInDocument) {
+        const spinner = screen.getByRole('presentation');
+        expect(spinner).toBeInTheDocument();
+      } else {
+        const spinner = screen.queryByRole('presentation');
+        expect(spinner).not.toBeInTheDocument();
       }
-    );
+    });
+  });
+
+  describe('Loader render', () => {
+    it('should render Loader inside Modal when promiseInProgress is true', () => {
+      //Arrange
+      mockUsePromiseTracker.mockReturnValue({ promiseInProgress: true });
+
+      //Act
+      render(<SpinnerComponent />);
+
+      //Assert
+      const loader = screen.getByTestId('loader-wrapper');
+      expect(loader).toBeInTheDocument();
+    });
   });
 });
