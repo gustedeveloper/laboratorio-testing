9c8bd8eb3d89a73a571681d0cb9ebbe989548ce5
Guste Gaubaite <219.guste@gmail.com>
2025-06-26 11:49:46 +0200

    Update ci file and config for e2e tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:5174#',
+    baseUrl: 'http://localhost:5173#',
     specPattern: 'cypress/e2e/**/*.spec.{js,jsx,ts,tsx}',
+    video: true,
+    screenshotOnRunFailure: true,
   },
 });
